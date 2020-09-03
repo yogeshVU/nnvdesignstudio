867233f74cf69adbaf74791cfa3609828bd23334
yogeshvu <yogesh@yogesh.com>
2020-09-03 10:42:22 -0500

    initial commit

---
+++ src/plugins/MyPythonPlugin/MyPythonPlugin/__init__.py
@@ -0,0 +1,28 @@
+"""
+This is where the implementation of the plugin code goes.
+The MyPythonPlugin-class is imported from both run_plugin.py and run_debug.py
+"""
+import sys
+import logging
+from webgme_bindings import PluginBase
+
+# Setup a logger
+logger = logging.getLogger('MyPythonPlugin')
+logger.setLevel(logging.DEBUG)
+handler = logging.StreamHandler(sys.stdout)  # By default it logs to stderr..
+handler.setLevel(logging.DEBUG)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+
+
+class MyPythonPlugin(PluginBase):
+    def main(self):
+        core = self.core
+        root_node = self.root_node
+        active_node = self.active_node
+        name = core.get_attribute(active_node, 'name')
+        logger.info('ActiveNode at "{0}" has name {1}'.format(core.get_path(active_node), name))
+        # core.set_attribute(active_node, 'name', 'newName')
+        commit_info = self.util.save(root_node, self.commit_hash, 'master', 'Python plugin updated the model')
+        logger.info('committed :{0}'.format(commit_info))
